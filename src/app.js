e79160cfd6fd99045acf8da4cae55e13c0a9510b
fgvies <facundovies@gmail.com>
2023-10-17 19:24:05 -0300

    configuraciones iniciales

--- src/app.js
+++ src/app.js
@@ -1,26 +1,30 @@
-const express = require("express");
-const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
-const routes = require("./routes/index.js");
-const cors = require("cors");
+import express from "express";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+import routes from "./routes/index.js";
+import cors from "cors";
+import mongoose from "mongoose";
 
-require("./db.js");
+const PORT = process.env.PORT || 3001;
 
-const server = express();
-server.name = "API";
+const connection = mongoose.connect(
+  "mongodb+srv://facundovies:123@cluster0.3geflhe.mongodb.net/MongoAvanzado?retryWrites=true&w=majority"
+);
 
-server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
-server.use(bodyParser.json({ limit: "50mb" }));
-server.use(cookieParser());
-server.use(morgan("dev"));
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(cookieParser());
+app.use(morgan("dev"));
 
 const corsOptions = {
-    origin: "*", //TODO: update to deployment url
-    credentials: true,
+  origin: "*", //TODO: update to deployment url
+  credentials: true,
 };
 
-server.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
 /* server.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
@@ -35,16 +39,16 @@ server.use(cors(corsOptions));
     );
     next();
 }); */
-
-server.use(routes);
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
 
 // Error catching endware.
-server.use((err, req, res, next) => {
-    // eslint-disable-line no-unused-vars
-    const status = err.status || 500;
-    const message = err.message || err;
-    console.error(err);
-    res.status(status).send(message);
+app.use((err, req, res, next) => {
+  // eslint-disable-line no-unused-vars
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.error(err);
+  res.status(status).send(message);
 });
+app.use(routes);
 
-module.exports = server;
+export default app;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,13 @@
-const { Router } = require("express");
+import  { Router } from "express";
+import userRouter from "./usersRouter.js";
+import productsRouter from "./productsRouter.js";
+import cartsRouter from "./cartsRouter.js";
 const router = Router();
 
-module.exports = router;
+
+router.use('/users', userRouter);
+router.use('/products', productsRouter)
+router.use('carts', cartsRouter)
+
+
+export default router;
